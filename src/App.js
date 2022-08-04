93b270bc9867b4c13759e3a715b15a6b8211dfd7
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-04 14:45:40 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,141 +1,45 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Card from 'react-credit-cards'
-import './styles.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import PaymentForm from "./PaymentForm";
 
-import {
-  formatCreditCardNumber,
-  formatCVC,
-  formatExpirationDate
-} from './utils'
-
-import 'react-credit-cards/es/styles-compiled.css'
-
-export default class App extends React.Component {
-  state = {
-    number: '',
-    name: '',
-    expiry: '',
-    cvc: '',
-    issuer: '',
-    focused: '',
-    formData: null
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    position: "absolute",
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 800,
+    maxHeight: 350
+  },
+  canvas1: {
+    position: "relative",
+    backgroundColor: "lightGrey",
+    zIndex: 0,
+    maxWidth: 350,
+    maxHeight: 230,
+    borderRadius: 10,
+    left: 50,
+    top: 20
+  },
+  canvas2: {
+    position: "absolute",
+    backgroundColor: "grey",
+    zIndex: 1,
+    maxWidth: 350,
+    maxHeight: 230,
+    top: 0,
+    left: 0,
+    borderRadius: 10
   }
+}));
 
-  handleCallback = ({ issuer }, isValid) => {
-    if (isValid) {
-      this.setState({ issuer })
-    }
-  }
-
-  handleInputFocus = ({ target }) => {
-    this.setState({
-      focused: target.name
-    })
-  }
-
-  handleInputChange = ({ target }) => {
-    if (target.name === 'number') {
-      target.value = formatCreditCardNumber(target.value)
-    } else if (target.name === 'expiry') {
-      target.value = formatExpirationDate(target.value)
-    } else if (target.name === 'cvc') {
-      target.value = formatCVC(target.value)
-    }
-
-    this.setState({ [target.name]: target.value })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    alert('You have finished payment!')
-    this.form.reset()
-  }
-
-  render () {
-    const { name, number, expiry, cvc, focused, issuer } = this.state
-
-    return (
-      <div key='Payment'>
-        <div className='App-payment'>
-          <h1>Enter your payment details</h1>
-          <h4>please input your information below</h4>
-          <Card
-            number={number}
-            name={name}
-            expiry={expiry}
-            cvc={cvc}
-            focused={focused}
-            callback={this.handleCallback}
-          />
-          <form ref={c => (this.form = c)} onSubmit={this.handleSubmit}>
-            <div className='form-group'>
-              <small>Name on card:</small>
-
-              <input
-                type='text'
-                name='name'
-                className='form-control'
-                placeholder='Name'
-                pattern='[a-z A-Z-]+'
-                required
-                onChange={this.handleInputChange}
-                onFocus={this.handleInputFocus}
-              />
-            </div>
-            <div className='form-group'>
-              <small>Card Number:</small>
-
-              <input
-                type='tel'
-                name='number'
-                className='form-control'
-                placeholder='Card Number'
-                pattern='[\d| ]{16,22}'
-                maxLength='19'
-                required
-                onChange={this.handleInputChange}
-                onFocus={this.handleInputFocus}
-              />
-            </div>
-
-            <div className='form-group'>
-              <small>Expiration Date:</small>
-
-              <input
-                type='tel'
-                name='expiry'
-                className='form-control'
-                placeholder='Valid Thru'
-                pattern='\d\d/\d\d'
-                required
-                onChange={this.handleInputChange}
-                onFocus={this.handleInputFocus}
-              />
-            </div>
-            <div className='form-group'>
-              <small>CVC:</small>
-
-              <input
-                type='tel'
-                name='cvc'
-                className='form-control'
-                placeholder='CVC'
-                pattern='\d{3}'
-                required
-                onChange={this.handleInputChange}
-                onFocus={this.handleInputFocus}
-              />
-            </div>
-            <input type='hidden' name='issuer' value={issuer} />
-            <div className='form-actions'>
-              <button>Submit</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    )
-  }
+function App() {
+  const classes = useStyles();
+  return PaymentForm();
 }
 
-render(<App />, document.getElementById('root'))
+export default App;
